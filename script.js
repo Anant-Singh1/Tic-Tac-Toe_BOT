22d2022df34045942c2716f547a2c53f6062d612
Anant-Singh1 <97743499+Anant-Singh1@users.noreply.github.com>
2022-08-22 19:48:38 +0530

    Crete some logic of the game

---
+++ script.js
@@ -0,0 +1,27 @@
+var orignalBoard;
+const humanPlayer = '0';
+const aiPlayer = 'x';
+const winningCombination = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+const cells = document.querySelectorAll(".cell");
+//Starting the game
+startTheGame();
+
+function startTheGame() {
+    document.querySelector('.endgame').style.display = 'none';
+    orignalBoard = Array.from(Array(9).keys());
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].innerText = '';
+        cells[i].style.removeProperty('background-color');
+        cells[i].addEventListener('click', turnClick, false);
+    }
+}
